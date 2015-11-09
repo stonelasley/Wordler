c9cb51493dfc8d2a03fce4ca348f8f1a7fc6c4c3
Stone C. Lasley <stonelasley@gmail.com>
2015-11-08 18:28:25 -0700

    Graphing WIP

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/nvd3/build/nv.d3.min.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -15,7 +16,10 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-file-upload/angular-file-upload.js'
+        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/d3/d3.js',
+        'public/lib/nvd3/build/nv.d3.min.js',
+        'public/lib/angular-nvd3/dist/angular-nvd3.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
--- modules/pages/client/controllers/pages.client.controller.js
+++ modules/pages/client/controllers/pages.client.controller.js
@@ -2,83 +2,150 @@
 
 // Pages controller
 angular.module('pages').controller('PagesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Pages',
-  function ($scope, $stateParams, $location, Authentication, Pages) {
-    $scope.authentication = Authentication;
+    function ($scope, $stateParams, $location, Authentication, Pages) {
+        $scope.authentication = Authentication;
 
-    // Create new Page
-    $scope.create = function (isValid) {
-      $scope.error = null;
+        // Create new Page
+        $scope.create = function (isValid) {
+            $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'pageForm');
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'pageForm');
 
-        return false;
-      }
+                return false;
+            }
 
-      // Create new Page object
-      var page = new Pages({
-        url: this.url,
-        note: this.note
-      });
+            // Create new Page object
+            var page = new Pages({
+                url: this.url,
+                note: this.note
+            });
 
-      // Redirect after save
-      page.$save(function (response) {
-        $location.path('pages/' + response._id);
+            // Redirect after save
+            page.$save(function (response) {
+                $location.path('pages/' + response._id);
 
-        // Clear form fields
-        $scope.url = '';
-        $scope.note = '';
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
+                // Clear form fields
+                $scope.url = '';
+                $scope.note = '';
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-    // Remove existing Page
-    $scope.remove = function (page) {
-      if (page) {
-        page.$remove();
+        // Remove existing Page
+        $scope.remove = function (page) {
+            if (page) {
+                page.$remove();
 
-        for (var i in $scope.pages) {
-          if ($scope.pages[i] === page) {
-            $scope.pages.splice(i, 1);
-          }
-        }
-      } else {
-        $scope.page.$remove(function () {
-          $location.path('pages');
-        });
-      }
-    };
+                for (var i in $scope.pages) {
+                    if ($scope.pages[i] === page) {
+                        $scope.pages.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.page.$remove(function () {
+                    $location.path('pages');
+                });
+            }
+        };
 
-    // Update existing Page
-    $scope.update = function (isValid) {
-      $scope.error = null;
+        // Update existing Page
+        $scope.update = function (isValid) {
+            $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'pageForm');
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'pageForm');
 
-        return false;
-      }
+                return false;
+            }
 
-      var page = $scope.page;
+            var page = $scope.page;
 
-      page.$update(function () {
-        $location.path('pages/' + page._id);
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
+            page.$update(function () {
+                $location.path('pages/' + page._id);
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-    // Find a list of Pages
-    $scope.find = function () {
-      $scope.pages = Pages.query();
-    };
+        // Find a list of Pages
+        $scope.find = function () {
+            $scope.pages = Pages.query();
+        };
 
-    // Find existing Page
-    $scope.findOne = function () {
-      $scope.page = Pages.get({
-        pageId: $stateParams.pageId
-      });
-    };
-  }
+        // Find existing Page
+        $scope.findOne = function () {
+            $scope.page = Pages.get({
+                pageId: $stateParams.pageId
+            });
+        };
+
+        // Set Chart Options
+        $scope.options = {
+            chart: {
+                type: 'discreteBarChart',
+                height: 450,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 50,
+                    left: 55
+                },
+                x: function (d) {
+                    return d.k;
+                },
+                y: function (d) {
+                    return d.v;
+                },
+                showValues: true,
+                valueFormat: function (d) {
+                    //return d3.format('d')(d);
+                    return d;
+                },
+                transitionDuration: 500,
+                xAxis: {
+                    axisLabel: 'Word',
+                    rotateLabels: 30
+                },
+                yAxis: {
+                    axisLabel: 'Occurence',
+                    axisLabelDistance: -10
+                }
+            }
+        };
+
+        // Set Chart Data
+        $scope.data = [{
+            key: "Cumulative Return",
+            //values: $scope.page.words
+            ///values: [{"k":"the","v":12},{"k":"and","v":12},{"k":"said","v":3},{"k":"top","v":3},{"k":"united","v":2},{"k":"states","v":2},{"k":"will","v":2},{"k":"china","v":2},{"k":"saturday","v":5},{"k":"for","v":4},{"k":"video","v":3},{"k":"southeast","v":2},{"k":"that","v":2},{"k":"was","v":2},{"k":"presidential","v":2},{"k":"such","v":2},{"k":"their","v":2},{"k":"first","v":2},{"k":"they","v":2},{"k":"from","v":3},{"k":"full","v":2},{"k":"news","v":8},{"k":"world","v":2},{"k":"business","v":2},{"k":"reuters","v":3},{"k":"delayed","v":2},{"k":"least","v":2},{"k":"minutes","v":2}]
+            values: [
+                {"k": "the", "v": 12},
+                {"k": "and", "v": 12},
+                {"k": "said", "v": 3},
+                {"k": "top", "v": 3},
+                {
+                    "k": "united", "v": 2
+                }, {"k": "states", "v": 2}, {"k": "will", "v": 2}, {"k": "china", "v": 2}, {
+                    "k": "saturday",
+                    "v": 5
+                }, {"k": "for", "v": 4}, {"k": "video", "v": 3}, {"k": "southeast", "v": 2}, {
+                    "k": "that",
+                    "v": 2
+                }, {"k": "was", "v": 2}, {"k": "presidential", "v": 2}, {"k": "such", "v": 2}, {
+                    "k": "their",
+                    "v": 2
+                }, {"k": "first", "v": 2}, {"k": "they", "v": 2}, {"k": "from", "v": 3}, {"k": "full", "v": 2}, {
+                    "k": "news",
+                    "v": 8
+                }, {"k": "world", "v": 2}, {"k": "business", "v": 2}, {"k": "reuters", "v": 3}, {
+                    "k": "delayed",
+                    "v": 2
+                }, {"k": "least", "v": 2}, {"k": "minutes", "v": 2}
+            ]
+        }];
+
+
+    }
 ]);
